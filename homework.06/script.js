27749fd3dd43c40a2d4c171c3b22ddb3b9545f89
Serhii <kozlovskijserhij@gmail.com>
2023-01-13 15:32:08 +0200

    add slider

---
+++ homework.06/script.js
@@ -0,0 +1,10 @@
+
+	   $(".slider-first").slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      prevArrow: $(".prev"),
+      nextArrow: $(".next"),
+      adaptiveHeight: true,
+   });
